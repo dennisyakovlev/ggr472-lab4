7dc10a046e00813e6611744e5f1cf00bfef525b2
Dennis Yakovlev <dennis.yakovlev@yahoo.ca>
2024-03-19 16:55:23 -0400

    final

---
+++ button.js
@@ -0,0 +1,125 @@
+const _animatables = {};
+
+class Animatable
+{
+    _onClick(e)
+    {
+        const funcs = this.state%2==1 ? this.offEvents : this.onEvents;
+        this.state += 1;
+        for (const [id,func] of Object.entries(funcs))
+        {
+            func(e, this.state)
+        }
+    }
+
+    constructor(id)
+    {
+        this.id = id;
+        this.onEvents = {}
+        this.offEvents = {}
+        this.onIds = 0;
+        this.offIds = 0;
+        this.state = 0;
+        _animatables[this.id] = this;
+
+        // we want to pass down e so forced to use function(e) instead of e =>
+        //      as using this in later gives class instance
+        $(`#${this.id}`).on('click', function(e) {
+            const id = $(this).attr('id');
+            if (id in _animatables) _animatables[id]._onClick(e);
+            else throw new Error(`didn't find ${id} in _animatables`)
+        });
+    }
+
+    addOnFunc(func)
+    {
+        this.onEvents[this.onIds] = func;
+        const oldId = this.onIds;
+        this.onIds += 1;
+        return oldId;
+    }
+
+    addOffFunc(func)
+    {
+        this.offEvents[this.offIds] = func;
+        const oldId = this.offIds;
+        this.offIds += 1;
+        return oldId;
+    }
+
+    removeOnFunc(id)
+    {
+        if (!(id in this.onEvents)) throw new Error(`key ${id} not in on funcs`)
+        delete this.onEvents[id]
+    }
+
+    removeOnFunc(id)
+    {
+        if (!(id in this.offEvents)) throw new Error(`key ${id} not in off funcs`)
+        delete this.offEvents[id]
+    }
+};
+
+class Button extends Animatable
+{
+    _transitionFunction(e, state)
+    {   
+        const children = $(e.currentTarget).children();
+        const text1 = children[0];
+        const text2 = children[1];
+
+        if (state%2==1)
+        {
+            $(text1)
+                .removeClass(`text-anim-start text-anim-in`)
+                .addClass(`text-anim-mid text-anim-out`);
+            $(text2)
+                .removeClass(`text-anim-out`)
+                .addClass(`text-anim-in`);
+        }
+        else
+        {
+            $(text1)
+                .removeClass(`text-anim-mid text-anim-out`)
+                .addClass(`text-anim-start text-anim-in`);
+            $(text2)
+                .removeClass(`text-anim-in`)
+                .addClass(`text-anim-out`);
+        }
+    }
+
+    enableAnim()
+    {
+        this.addOnFunc(this._transitionFunction);
+        this.addOffFunc(this._transitionFunction)
+    }
+};
+
+class Menu extends Animatable
+{
+    _transitionFunction(e, state)
+    {
+        const children = $(e.currentTarget).parent().children();
+        const content = children[1]; 
+        console.log(e)
+        if (state%2==1)
+        {
+            console.log(content, e)
+            $(content)
+                .removeClass('menu-anim-mid menu-anim-out no-pointer')
+                .addClass(`menu-anim-in menu-anim-start`);
+        }
+        else
+        {
+            $(content)
+                .removeClass('menu-anim-start menu-anim-in')
+                .addClass('menu-anim-mid menu-anim-out no-pointer');
+        }
+    }
+
+    enableAnim()
+    {
+        this.addOnFunc(this._transitionFunction);
+        this.addOffFunc(this._transitionFunction);
+    }
+};
--- script.js
+++ script.js
@@ -12,8 +12,9 @@ mapboxgl.accessToken = "pk.eyJ1IjoiZGVubmlzeWFrb3ZsZXY0MCIsImEiOiJjbHMyNnViazIwM
 const map = new mapboxgl.Map({
     container: 'map', // container id in HTML
     style: 'mapbox://styles/dennisyakovlev40/clskppghq03u401p2c3184488',  // ****ADD MAP STYLE HERE *****
-    center: [-79.39, 43.65],  // starting point, longitude/latitude
-    zoom: 12 // starting zoom level
+    center: [-79.39, 43.72],  // starting point, longitude/latitude
+    // cetner: [-74.35, 40.70],
+    zoom: 10 // starting zoom level
 });
 
 /*--------------------------------------------------------------------
@@ -24,32 +25,148 @@ Step 2: VIEW GEOJSON POINT DATA ON MAP
 //      Convert the response to JSON format and then store the response in your new variable
 
 
-/*--------------------------------------------------------------------
-    Step 3: CREATE BOUNDING BOX AND HEXGRID
---------------------------------------------------------------------*/
-//HINT: All code to create and view the hexgrid will go inside a map load event handler
-//      First create a bounding box around the collision point data then store as a feature collection variable
-//      Access and store the bounding box coordinates as an array variable
-//      Use bounding box coordinates as argument in the turf hexgrid function
+var crashes_mine;
+
+fetch('https://raw.githubusercontent.com/dennisyakovlev/ggr472-lab4/master/data/pedcyc_collision_06-21.geojson')
+    .then(response => response.json())
+    .then(response => {
+        crashes_mine = response;
+    })
+    .catch(err => {
+        throw new Error('Problem loading');
+    });
+
+function dataParse(data)
+{
+    const features = turf.featureCollection(data.features);
+    const bbox = turf.envelope(features);
+    const poly = turf.polygon(bbox.geometry.coordinates);
+    const bboxTrans = turf.transformScale(poly.geometry, 1.2);
+    const newBbox = turf.envelope(bboxTrans);
+    const hexGrid = turf.hexGrid(newBbox.bbox, 0.5, 'kilometers');
+    const collected = turf.collect(hexGrid, data, "_id", "values")
 
 
+    const shouldPick = (i, colSz) => {
+        return  collected.features[i - colSz].properties.values.length     +   // left
+                collected.features[i - colSz - 1].properties.values.length +   // left
+                collected.features[i - colSz + 1].properties.values.length +   // left
 
-/*--------------------------------------------------------------------
-Step 4: AGGREGATE COLLISIONS BY HEXGRID
---------------------------------------------------------------------*/
-//HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
-//      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
+                collected.features[i - 1].properties.values.length           + // up
+                collected.features[i + 1].properties.values.length           + // down
 
+                collected.features[i + colSz - 1].properties.values.length +   // right
+                collected.features[i + colSz + 1].properties.values.length +   // right
+                collected.features[i + colSz].properties.values.length;        // right
+    };
+    
 
+    const colSize = 38;
+    const rowSize = 63;
+    let arr = [];
+    // cols alternate in 38,37 size
+    // ignore first 2 + last col, they dont have data anyways
+    for (var col=2, i=colSize*2-1; col!=rowSize-1; ++col)
+    {
+        // i+=1;
+        for (var row=0; row!=colSize-(col%2); ++row,++i)
+        {
+            console.log(i)
+            if (shouldPick(i, colSize-(col%2)) > 0)
+            {
+                arr.push(collected.features[i]);
+            }
+        }
+    }
+    collected.features = arr;
 
-// /*--------------------------------------------------------------------
-// Step 5: FINALIZE YOUR WEB MAP
-// --------------------------------------------------------------------*/
-//HINT: Think about the display of your data and usability of your web map.
-//      Update the addlayer paint properties for your hexgrid using:
-//        - an expression
-//        - The COUNT attribute
-//        - The maximum number of collisions found in a hexagon
-//      Add a legend and additional functionality including pop-up windows
+    return collected
+}
 
+map.on('load', () => {
 
+    const processed_THREE = dataParse(crashes_mine);
+
+    map.addSource('crashes', {
+        type: 'geojson',
+        data: crashes_mine
+    });
+
+    map.addSource('hexGrid', {
+        type: 'geojson',
+        data: processed_THREE
+    });
+
+    map.addLayer({
+        id: 'crashesHexGrid',
+        type: 'fill',
+        source: 'hexGrid',
+        layout: {},
+        paint: {
+            'fill-color': [
+                'case',
+                ['<=', ['length', ['get', 'values']], 0], '#2D00F7',
+                ['<=', ['length', ['get', 'values']], 2], '#6A00F4',
+                ['<=', ['length', ['get', 'values']], 4], '#8900F2',
+                ['<=', ['length', ['get', 'values']], 6], '#A100F2',
+                ['<=', ['length', ['get', 'values']], 8], '#B100E8',
+                ['<=', ['length', ['get', 'values']], 10], '#BC00DD',
+                ['<=', ['length', ['get', 'values']], 12], '#D100D1',
+                ['<=', ['length', ['get', 'values']], 16], '#DB00B6',
+                ['<=', ['length', ['get', 'values']], 20], '#E500A4',
+                ['<=', ['length', ['get', 'values']], 100], '#F20089',
+                'red'
+            ],
+            'fill-opacity': 0.6
+        }
+    });
+
+    // points ontop of hex grid
+    map.addLayer({
+        id: 'crashesPoint',
+        type: 'circle',
+        source: 'crashes',
+        paint: {
+            'circle-color': 'orange',
+            'circle-opacity': 0,
+            'circle-radius': 5,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': 'black',
+            'circle-stroke-opacity': 0
+        },
+        visibility: 'none'
+    });
+
+    const menu = new Menu('menu-1');
+    menu.enableAnim();
+
+    // NOTE: set the visibility to none/visible since this improves
+    //       the map performance
+    //       this current messes up animation of layers, later use
+    //       TransitionEndListener to set this nicely after anim end
+
+    const buttonAccidents = new Button('btn-2');
+    buttonAccidents.enableAnim();
+    buttonAccidents.addOnFunc((e,state) => {
+        // map.setLayoutProperty('crashesPoint', 'visibility', 'visible');
+        map.setPaintProperty('crashesPoint', 'circle-opacity', 1);
+        map.setPaintProperty('crashesPoint', 'circle-stroke-opacity', 1);
+    });
+    buttonAccidents.addOffFunc((e,state) => {
+        // map.setLayoutProperty('crashesPoint', 'visibility', 'none');
+        map.setPaintProperty('crashesPoint', 'circle-opacity', 0);
+        map.setPaintProperty('crashesPoint', 'circle-stroke-opacity', 0);
+    });
+
+    const buttonHexgrid = new Button('btn-3');
+    buttonHexgrid.enableAnim();
+    buttonHexgrid.addOnFunc((e,state) => {
+        // map.setLayoutProperty('crashesHexGrid', 'visibility', 'none');
+        map.setPaintProperty('crashesHexGrid', 'fill-opacity', 0);
+    });
+    buttonHexgrid.addOffFunc((e,state) => {
+        // map.setLayoutProperty('crashesHexGrid', 'visibility', 'visible');
+        map.setPaintProperty('crashesHexGrid', 'fill-opacity', 0.6);
+    });
+
+});
